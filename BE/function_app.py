28397db5807f252115a735d0ce879f8a8c4aed24
anhquan1301 <nlaquan1301@gmail.com>
2023-12-21 01:43:17 +0700

    update 21/12

--- BE/function_app.py
+++ BE/function_app.py
@@ -1,13 +1,14 @@
 from http import HTTPStatus
 import azure.functions as func
-import datetime
-import json
 import logging
+
+from fastapi import FastAPI
 from controller.AuthController import AuthController
 from common.Commons import Commons
 from config.db import SessionLocal
 from controller.CustomerController import CustomerController
 from controller.ProductController import ProductController
+from fastapi.middleware.cors import CORSMiddleware
 
 app = func.FunctionApp()
 db = SessionLocal()
@@ -25,7 +26,7 @@ def route_dispatcher(req: func.HttpRequest) -> func.HttpResponse:
             "register": AuthController(db=db).register,
             "create-product": ProductController(db=db).create_product,
             "create-product-capacity": ProductController(db=db).create_product_capacity,
-            "create-customer":CustomerController(db=db).create_customer,
+            "create-customer": CustomerController(db=db).create_customer,
         }
     elif req.method == "PUT":
         routes = {"change-password": AuthController(db=db).change_password}
@@ -33,12 +34,13 @@ def route_dispatcher(req: func.HttpRequest) -> func.HttpResponse:
         routes = {}
     elif req.method == "DELETE":
         routes = {}
-    else:
-        error_message = Commons(db=db).get_message("Method not supported")
-        return Commons(db=db).response_func_http(error_message, HTTPStatus.BAD_REQUEST)
+    # else:
+    #     error_message = Commons(db=db).get_message("Method not supported")
+    #     return Commons(db=db).response_func_http(error_message, HTTPStatus.BAD_REQUEST)
     route = req.route_params.get("route")
     if route in routes:
-        return routes[route](req)
+        response = routes[route](req)
+        return response
     else:
         error_message = Commons(db=db).get_message("Route not found")
         return Commons(db=db).response_func_http(error_message, HTTPStatus.NOT_FOUND)
@@ -46,9 +48,17 @@ def route_dispatcher(req: func.HttpRequest) -> func.HttpResponse:
 
 @app.route(route="{route}")
 def main(req: func.HttpRequest) -> func.HttpResponse:
+    headers = {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "OPTIONS, POST, GET, PUT, DELETE",
+        "Access-Control-Allow-Headers": "Content-Type",
+    }
     try:
         logging.info(req)
-        return route_dispatcher(req)
+        response = route_dispatcher(req)
+        return func.HttpResponse(
+            body=response.get_body(), headers=headers, status_code=response.status_code
+        )
     except Exception as e:
         logging.error(e)
         error_message = {"message": e.args[0]["message"]}
